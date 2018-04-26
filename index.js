244db5aa99db5e2e849a4988d6eed965723d00aa
MonkeyNan <2576566794@qq.com>
2018-04-26 15:36:41 +0800

    hello world
    hello world

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+import san from 'san';
+
+var MyApp = san.defineComponent({
+    template: '<p>Hello {{name}}!</p>',
+
+    initData: function () {
+        return {
+            name: 'world'
+        };
+    }
+});
+
+
+var myApp = new MyApp();
+myApp.attach(document.body);
